06f9528e7b9fb868951f2acb7bb0405cae958d83
Vitamin <vitamin@ukr.net>
2018-09-13 17:32:24 +0300

    2

--- HW3/js/map.js
+++ HW3/js/map.js
@@ -1,18 +1,34 @@
-const Map = (elem, func) => {
+
+const arr = [5, 7, 3, 2, 1];
+
+
+const obj = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+
+};
+
+const myFunc = x => x*2;
+
+
+const myMap = (elem, func) => {
     if(elem instanceof Array){
-        const newArr = [...elem];
-        for(let i=0; i<newArr.length; i++){
-            newArr[i] = func(newArr[i]);
-        }
-        return newArr;
+        if (!elem.length) return [];
+        let [x, ...rest] = elem;
+        return [func(x), ...myMap(rest, func)];
     }else if (elem instanceof Object){
         const newObj = {...elem}
-
         for(let key in newObj){
-            newObj[key] = func(newObj[key]);
+             newObj[key] = func(newObj[key]);
         }
         return newObj;
     }else {
-        return func(elem);
+         return func(elem);
     }
 }
+
+console.log('myMap', myMap(arr, myFunc));
+console.log('----------------------------------------------');
+console.log('myMap', myMap(obj, myFunc));
