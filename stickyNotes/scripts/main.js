1bda4cd88393533bcfb3f436d57ff2b50eae3104
Vitamin <vitamin@ukr.net>
2018-10-03 15:01:48 +0300

    1

--- stickyNotes/scripts/main.js
+++ stickyNotes/scripts/main.js
@@ -3,12 +3,20 @@ class StickyNotesApp {
         this.noteMessageInput = document.getElementById('message');
         this.addNoteButton = document.getElementById('save');
         this.notesSectionTitle = document.querySelector('.first');
-
         this.addNoteButton.addEventListener('click', () => this.saveNote());
         this.noteMessageInput.addEventListener('keyup', () => this.toggleButton());
         for (let key in localStorage) {
             if(!isNaN(parseInt(key))) this.displayNote(key, localStorage[key]);
         }
+        this.dragSrcEl = null;
+        this.containers = document.querySelectorAll('.cont');
+        this.containers.forEach( container => {
+            container.addEventListener('dragenter', this.handleDragEnter, false);
+            container.addEventListener('dragover', this.handleDragOver, false);
+            container.addEventListener('dragleave', this.handleDragLeave, false);
+            container.addEventListener('drop', (e) => this.handleDrop(e, this.dragSrcEl), false);
+        });
+
     }
     saveNote() {
         if (this.noteMessageInput.value) {
@@ -24,12 +32,38 @@ class StickyNotesApp {
         if (!note) {
             note = document.createElement('div');
             note.id = key;
-            this.notesSectionTitle.appendChild(note);
+            note.className = "yellow  stickyNote";
+            note.draggable = true;
+            let date;
+            if (note.id) {
+                date = new Date(parseInt(note.id));
+            } else {
+                date = new Date();
+            }
+            let shortDate = new Intl.DateTimeFormat("en-US", {day: 'numeric', month: 'short'}).format(date);
+            const data = document.createElement('div');
+            data.className = "date";
+            data.innerHTML = `Created on ${shortDate}`;
+            note.appendChild(data);
+            const mess = document.createElement('div');
+            mess.className = "message";
+            mess.innerHTML = message;
+            note.appendChild(mess);
+            const btn = document.createElement('button');
+            btn.className = "btn text-right delete";
+            btn.innerHTML = "Delete";
+
+            note.addEventListener('dragstart', (e) => this.handleDragStart(e, this.dragSrcEl), false);
+            note.addEventListener('dragend', this.handleDragEnd, false);
+
+            note.appendChild(btn);
+            this.notesSectionTitle.insertBefore(note, this.notesSectionTitle.firstChild);
+            this.deleteButton = note.querySelector('.delete');
+            this.deleteButton.addEventListener('click', () => this.deleteNote(note));
         }
         if (!message) {
-            return note.deleteNote();
+            return () => this.deleteNote(note);
         }
-        note.setMessage(message);
     };
     toggleButton() {
         if (this.noteMessageInput.value) {
@@ -38,68 +72,59 @@ class StickyNotesApp {
             this.addNoteButton.setAttribute('disabled', 'true');
         }
     };
+    deleteNote(note) {
+        localStorage.removeItem(note.id);
+        note.parentNode.removeChild(note);
+    };
+//    Drag and Drop
+    handleDragStart(e, dragSrcEl) {
+        this.style.opacity = '0.4';  // this / e.target is the source node.
+        this.dragSrcEl = this;
+        e.dataTransfer.effectAllowed = 'move';
+        // e.dataTransfer.setData('text/html', this.innerHTML);
+        setTimeout(() => this.style.display = "none", 0);
+    }
+    handleDragOver(e) {
+        if (e.preventDefault) {
+            e.preventDefault(); // Necessary. Allows us to drop.
+        }
+        e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+        return false;
+    }
+    handleDragEnter(e) {
+        // this / e.target is the current hover target.
+        this.classList.add('over');
+    }
+    handleDragLeave(e) {
+        this.classList.remove('over');  // this / e.target is previous target element.
+    }
+    handleDrop(e, dragSrcEl) {
+        if (e.stopPropagation) {
+            e.stopPropagation(); // Stops some browsers from redirecting.
+        }
+        // Don't do anything if dropping the same column we're dragging.
+        console.log("this.dragSrcEl - end", dragSrcEl);
+        // Set the source column's HTML to the HTML of the column we dropped on.
+        if (dragSrcEl != this) {
+            // dragSrcEl.innerHTML = this.innerHTML;
+            // this.innerHTML = e.dataTransfer.getData('text/html');
+            console.log("this", this);
+            // console.log("this22", this.innerHTML);
+
+        }
+        return false;
+    }
+    handleDragEnd(e) {
+        // this/e.target is the source node.
+        document.querySelectorAll('.stickyNote')
+        .forEach((note) => {
+            note.style.opacity = "1";
+        });
+        document.querySelectorAll('.cont').forEach(cont => cont.classList.remove('over'));
+        this.style.display = "block";
+    }
+
 }
 
-// On load start the app.
 window.addEventListener('load', () => new StickyNotesApp() );
 
-// A Sticky Note custom element that extends HTMLElement.
-var StickyNote = Object.create(HTMLElement.prototype);
-
-// Initial content of the element.
-StickyNote.TEMPLATE =
-    `<div class="yellow  stickyNote">
-        <div class="message"></div>
-        <div class="date"></div>
-        <button class="btn text-right delete">Delete</button>
-     </div>`;
-
-// StickyNote elements top level style classes.
-StickyNote.CLASSES = ['gray', 'mb-5', 'mt-5'];
-
-// List of shortened month names.
-StickyNote.MONTHS = [ 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Ноябрь', 'Декабрь' ];
-
-
-// Fires when an instance of the element is created.
-StickyNote.createdCallback = function() {
-    StickyNote.CLASSES.forEach(function(klass) {
-        this.classList.add(klass);
-    }.bind(this));
-    this.innerHTML = StickyNote.TEMPLATE;
-    this.messageElement = this.querySelector('.message');
-    this.dateElement = this.querySelector('.date');
-    this.deleteButton = this.querySelector('.delete');
-    this.deleteButton.addEventListener('click', this.deleteNote.bind(this));
-};
-
-// Fires when an attribute of the element is added/deleted/modified.
-StickyNote.attributeChangedCallback = function(attributeName) {
-    // We display/update the created date message if the id changes.
-    if (attributeName == 'id') {
-        if (this.id) {
-            var date = new Date(parseInt(this.id));
-        } else {
-            var date = new Date();
-        }
-        var month = StickyNote.MONTHS[date.getMonth()];
-        this.dateElement.textContent = 'Created on ' + month + ' ' + date.getDate();
-    }
-};
-
-// Sets the message of the note.
-StickyNote.setMessage = function(message) {
-    this.messageElement.textContent = message;
-    // Replace all line breaks by <br>.
-    this.messageElement.innerHTML = this.messageElement.innerHTML.replace(/\n/g, '<br>');
-};
-
-// Deletes the note by removing the element from the DOM and the data from localStorage.
-StickyNote.deleteNote = function() {
-    localStorage.removeItem(this.id);
-    this.parentNode.removeChild(this);
-};
-
-// document.registerElement('div', {
-//     prototype: StickyNote
-// });
