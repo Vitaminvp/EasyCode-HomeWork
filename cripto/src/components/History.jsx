81ee527bbc41c01edca2e6f8003e23b79282ea60
Vitamin <vitamin@ukr.net>
2018-10-12 15:06:48 +0300

    1

--- cripto/src/components/History.jsx
+++ cripto/src/components/History.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
 
 class History extends Component {
     render() {
@@ -7,6 +7,7 @@ class History extends Component {
             <div>
                 <h1>History</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, dignissimos sit. Aperiam autem cum debitis doloremque libero qui, quia repellat rerum? Error maxime modi nisi nobis possimus quas quod rerum saepe voluptate voluptates? Facilis inventore itaque officia quae soluta? Aliquid assumenda explicabo natus optio porro possimus quaerat quia sit vitae!</p>
+                <Redirect to="/"/>
             </div>
         );
     }
