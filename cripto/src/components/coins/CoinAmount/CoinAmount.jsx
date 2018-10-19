78e362ae5bb463d3fbc9e876eb4e0030c48e9b46
Vitalii Vitaminenko <vitamin@ukr.net>
2018-10-19 23:00:20 +0300

    Merge branch 'master' of https://github.com/Vitaminvp/EasyCodeHW
    # Conflicts:
    #	stickyNotes/index.html
    #	stickyNotes/scripts/main.js

--- cripto/src/components/coins/CoinAmount/CoinAmount.jsx
+++ cripto/src/components/coins/CoinAmount/CoinAmount.jsx
@@ -4,17 +4,19 @@ import './coinAmount.css';
 class CoinAmount extends Component {
     handleChange = (e) => {
         const re = /^[0-9.\b]+$/;
-
         if (e.target.value === '' || re.test(e.target.value)) {
             this.props.handleCoinsChangeAmount(this.props.coin.Name, e.target.value);
-        }else e.target.value = '';
+        } else e.target.value = '';
         e.preventDefault();
-    }
+    };
+
     render() {
         const {coin, value} = this.props;
         return (
             <div className="coinAmount">
-                <label className="coinAmount_label"><span>{coin.Name}:</span> <input onChange={this.handleChange} defaultValue={value} className="coinAmount_input" /></label>
+                <label className="coinAmount_label"><span>{coin.Name}:</span> <input onChange={this.handleChange}
+                                                                                     defaultValue={value}
+                                                                                     className="coinAmount_input"/></label>
             </div>
         );
     }
