23b9c360dbe02409c865b6685954d6f20bc1a8d0
Vitalii Vitaminenko <vitamin@ukr.net>
2018-11-26 22:05:45 +0200

    The final

---
+++ cripto/docs/precache-manifest.5adba21e91f02992526b16d1a2aee4dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1911ec2fbebad0279515",
+    "url": "/static/js/main.1911ec2f.chunk.js"
+  },
+  {
+    "revision": "de31cb316b664f1a6f0d",
+    "url": "/static/js/1.de31cb31.chunk.js"
+  },
+  {
+    "revision": "1911ec2fbebad0279515",
+    "url": "/static/css/main.8647c8e7.chunk.css"
+  },
+  {
+    "revision": "de31cb316b664f1a6f0d",
+    "url": "/static/css/1.b63b9ec1.chunk.css"
+  },
+  {
+    "revision": "e3e55a7f902449080e8c863552240066",
+    "url": "/index.html"
+  }
+];
