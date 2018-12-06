01550f4ca857a63263e1f6fc3bc894d00b6bc32c
Vitamin <vitamin@ukr.net>
2018-12-06 11:12:16 +0200

    last

--- cripto/src/components/404.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ForOFor = () => (
-    <h1 className="text-center">
-        404
-    </h1>
-);
-
-export default ForOFor;
--- cripto/src/components/Price/CoinAmount/ErrorSpan/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ErrorSpan = ({children}) => <span className="warning alarm">{children}</span>;
-
-
-export default ErrorSpan;
-
